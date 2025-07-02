22fa5f554cdd7c35e5ce4bd52414859ca433a812
Rayan786-12 <rayanriaz1212@gmail.com>
2025-07-02 18:07:11 +0530

    Model Trainer

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,49 @@
+# import sys
+# from logger import logging
+
+# # logging.basicConfig(
+# #     filename='error.log',
+# #     filemode='a',  # Append mode (use 'w' to overwrite)
+# #     level=logging.INFO,
+# #     format='%(asctime)s - %(levelname)s - %(message)s'
+# # )
+# def error_message_detail(error,error_detail:sys):
+#     _,_,exc_tb=error_detail.exc_info()
+#     file_name=exc_tb.tb_frame.f_code.co_filename
+#     error_message="Error has occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+#         file_name,exc_tb.tb_lineno,str(error))
+
+#     return error_message
+
+
+# class CustomException(Exception):
+#     def __init__(self,error_message,error_detail:sys):
+#         super().__init__(error_message)
+#         self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+#     def __str__(self):
+#         return self.error_message
+    
+# if __name__=='__main__':
+#     try:
+#         a=1/0
+#     except ZeroDivisionError as e:
+#         logging.info("Divide by Zero")
+#         err= CustomException(e,sys)
+#         print(err)
+#         raise err
 import sys
 from src.logger import logging
 
-# logging.basicConfig(
-#     filename='error.log',
-#     filemode='a',  # Append mode (use 'w' to overwrite)
-#     level=logging.INFO,
-#     format='%(asctime)s - %(levelname)s - %(message)s'
-# )
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error has occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
+    
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -23,12 +52,3 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
-if __name__=='__main__':
-    try:
-        a=1/0
-    except ZeroDivisionError as e:
-        logging.info("Divide by Zero")
-        err= CustomException(e,sys)
-        print(err)
-        raise err
